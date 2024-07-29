10662faa878c3af77f031b6cb05cbc4d08de5b28
VALENTINA <valensoto486@gmail.com>
2024-07-29 17:05:42 -0500

    Estructura base dividida por paginas

--- JS/main.js
+++ JS/main.js
@@ -13,3 +13,16 @@ document.addEventListener('DOMContentLoaded', () => {
         form.reset(); // Limpia el formulario
     });
 });
+
+// Registro del service Worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
+            .then((registration) => {
+                console.log('Service Worker registrado con éxito:', registration);
+            })
+            .catch((error) => {
+                console.error('Error al registrar el Service Worker:', error);
+            });
+    });
+}
